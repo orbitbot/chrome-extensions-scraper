42a8a565aa9e7a2a0b4d41052703ae66d9d1e77e
Patrik Johnson <patrik@zebros.fi>
2013-11-14 00:21:22 +0200

    Working scraper with zip subfolder collision detection and workaround

--- scraper.py
+++ scraper.py
@@ -1,11 +1,20 @@
 from string import strip
+from operator import itemgetter
+import os
+import StringIO
+import zipfile
+import tempfile
+import shutil
 
 from bs4 import BeautifulSoup
 import requests
-import pprint
+
 
 base_url = 'http://developer.chrome.com/extensions/'
 page_url = base_url + 'samples.html'
+
+
+## Scrape data from Examples page
 
 r = requests.get(page_url)
 
@@ -17,9 +26,9 @@ for index, sample_section in enumerate(soup('div', class_='sample')):
     project = {}
     project['name'] = sample_section.a.string
     project['desc'] = (sample_section.a.parent.next_sibling).strip()
-    zipfile = sample_section.a.get('href')
-    project['zip'] = zipfile
-    project['folder'] = zipfile[ zipfile.rfind('/') + 1 : zipfile.rfind('.zip')]
+    zip_url = sample_section.a.get('href')
+    project['zip'] = zip_url
+    project['folder'] = zip_url[ zip_url.rfind('/') + 1 : zip_url.rfind('.zip')]
 
     doc = []
     files = []
@@ -37,10 +46,17 @@ for index, sample_section in enumerate(soup('div', class_='sample')):
 
     examples.append(project)
 
-pprint.pprint(examples[0])
+
+## Setup subfolder for new git repo
+
+subfolder = 'chrome-extensions-examples'
+
+os.mkdir(subfolder)
+
+
+## Download and extract example projects
 
 bullet_list_template = '* [%(call)s](%(link)s)'
-project_list_template = '* <a href="%(folder)s">%(name)s</a>'
 readme_template = """
 %(name)s
 =======
@@ -56,11 +72,87 @@ Calls
 
 """
 
-readme = readme_template % examples[0]
-bullets = []
-for item in examples[0]['doc']:
-    bullets.append(bullet_list_template % item)
+for project in examples:
 
-readme += '\n'.join(bullets)
+    project_path = os.path.join(subfolder, project['folder'])
+    
+    tmp_dir = tempfile.mkdtemp()
 
-print readme
+    r = requests.get(base_url + project['zip'])
+    if r.ok:
+        z = zipfile.ZipFile(StringIO.StringIO(r.content))
+        z.extractall(path=tmp_dir)
+
+    try:
+        shutil.copytree(os.path.join(tmp_dir, project['folder']), project_path)
+    except OSError, e:
+        if e.errno == 17:
+            num = 1
+            while True:
+                try:
+                    tmp_path = project_path + '_' + str(num)
+                    print 'Got existing subfolder: "' + project_path + '", trying "' + tmp_path + ""
+                    shutil.copytree(os.path.join(tmp_dir, project['folder']), tmp_path)
+                    project_path = tmp_path
+                    break
+                except OSError, e:
+                    if e.errno == 17:
+                        num += 1
+                        continue
+                    else:
+                        break
+                        raise e
+        else:
+            raise e
+
+    shutil.rmtree(tmp_dir)
+
+    project['subfolder'] = project_path
+
+    bullets = []
+    for item in project['doc']:
+        bullets.append(bullet_list_template % item)
+
+    readme = readme_template % project
+    readme += '\n'.join(bullets)
+
+    with open(os.path.join(project_path, 'README.md'), 'a') as outfile:
+        outfile.write(readme)
+
+
+## Write main readme with correct project subfolders
+
+project_list_template = '* <a href="%(subfolder)s">%(name)s</a>'
+project_list = []
+
+for project in sorted(examples, key = itemgetter('name')):
+    project_list.append(project_list_template % project)
+
+main_readme = """
+chrome-extensions-example
+=========================
+
+The [Chrome Extensions examples](http://developer.chrome.com/extensions/samples.html) did not 
+exist as a Git repository, and browsing both the samples page and the VCViewer did not seem particularly
+handy. So, I decided to scrape the content into this repository for easier browsing and (possible)
+editing.
+
+If you would like to clone a part of this repository, use git 
+[sparse checkouts](http://jasonkarns.com/blog/subdirectory-checkouts-with-git-sparse-checkout/).
+
+You can find the scraper used to generate this repository (except for a `git init` and push) 
+on (github](https://github.com/orbitbot/chrome-extension-scraper).
+
+
+Content is licensed under the [Google BSD License](http://code.google.com/google_bsd_license.html).
+
+
+Example projects
+----------------
+
+"""
+
+main_readme += '\n'.join(project_list)
+
+with open(os.path.join(subfolder, 'README.md'), 'w') as outfile:
+    outfile.write(main_readme)
